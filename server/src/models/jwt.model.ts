e3c08ca0236f7021bcf429b72d1f1116520a4b96
andrew-gagliostro <argagliostro@gmail.com>
2023-08-18 22:07:36 -0400

    removing server from frontend to move into amplify hosting for next

--- server/src/models/jwt.model.ts
+++
@@ -1,3 +0,0 @@
-export type token = {
-    id_token: string
-}
--- server/src/server.ts
+++
@@ -1,8 +0,0 @@
-import App from "./config/app";
-import logger from "./utils/logger.util";
-
-const PORT = process.env.PORT || 8080;
-
-App.listen(PORT, () => {
-  logger.info(null, `Server listening on port ${PORT}!`);
-});
