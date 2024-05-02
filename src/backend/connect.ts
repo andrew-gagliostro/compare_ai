f4f8764acff9e959ef264acded1ab249b7b3f6ca
andrew-gagliostro <argagliostro@gmail.com>
2024-05-01 20:57:01 -0400

    adding support for in-memory database ops

--- src/backend/connect.ts
+++ src/backend/connect.ts
@@ -1,37 +1,26 @@
-import mongoose from 'mongoose';
-import { Mutex } from 'async-mutex';
+import mongoose from "mongoose";
+import { Mutex } from "async-mutex";
+import { MongoMemoryServer } from "mongodb-memory-server";
 
-const {
-  MONGO_HOST,
-  MONGO_DB,
-  MONGO_USER,
-  MONGO_PASSWORD,
-} = process.env;
+const { MONGO_HOST, MONGO_DB, MONGO_USER, MONGO_PASSWORD } = process.env;
 
-const errorMessage = (missing: string) => `Please define the ${missing} environment variable inside .env.local`;
+const errorMessage = (missing: string) =>
+  `Please define the ${missing} environment variable inside .env.local`;
 
 if (!MONGO_HOST) {
-  throw new Error(
-    errorMessage('MONGO_HOST'),
-  );
+  throw new Error(errorMessage("MONGO_HOST"));
 }
 
 if (!MONGO_DB) {
-  throw new Error(
-    errorMessage('MONGO_DB'),
-  );
+  throw new Error(errorMessage("MONGO_DB"));
 }
 
 if (!MONGO_USER) {
-  throw new Error(
-    errorMessage('MONGO_USER'),
-  );
+  throw new Error(errorMessage("MONGO_USER"));
 }
 
 if (!MONGO_PASSWORD) {
-  throw new Error(
-    errorMessage('MONGO_PASSWORD'),
-  );
+  throw new Error(errorMessage("MONGO_PASSWORD"));
 }
 
 const mutex = new Mutex();
@@ -51,7 +40,7 @@ if (!cached) {
 
 let seeded = false;
 
-const connect = async () => {
+const connectToMongoDB = async () => {
   const release = await mutex.acquire();
   try {
     if (cached.conn) {
@@ -63,8 +52,7 @@ const connect = async () => {
 
       cached.promise = mongoose.connect(connectionString).then((res) => res);
       console.log(connectionString);
-      console.trace('CONNECTED TO MONGO');
-
+      console.trace("CONNECTED TO MONGO");
     }
 
     cached.conn = await cached.promise;
@@ -72,6 +60,25 @@ const connect = async () => {
   } finally {
     release();
   }
+};
+
+const connect = async () => {
+  // Determine whether to use the real database or the mock based on an environment variable or any other condition
+  if (process.env.USE_MOCK_DB) {
+    console.log("USING MOCK DB");
+    return connectToMockDB();
+  } else {
+    console.log("USING FULL DB");
+    return connectToMongoDB();
+  }
+};
+
+const connectToMockDB = async () => {
+  const mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
+  await mongoose.connect(uri);
+  console.log("Connected to Mock MongoDB");
+  return mongoose.connection;
 };
 
 export default connect;
