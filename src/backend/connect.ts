1d02762ec94abffdbd95399e254cbf99555c11fe
andrew-gagliostro <argagliostro@gmail.com>
2024-05-01 21:57:21 -0400

    testing using mongodb atlas connection string

--- src/backend/connect.ts
+++ src/backend/connect.ts
@@ -2,7 +2,13 @@ import mongoose from "mongoose";
 import { Mutex } from "async-mutex";
 import { MongoMemoryServer } from "mongodb-memory-server";
 
-const { MONGO_HOST, MONGO_DB, MONGO_USER, MONGO_PASSWORD } = process.env;
+const {
+  MONGO_HOST,
+  MONGO_DB,
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_CONNECTION_STRING,
+} = process.env;
 
 const errorMessage = (missing: string) =>
   `Please define the ${missing} environment variable inside .env.local`;
@@ -24,46 +30,40 @@ if (!MONGO_PASSWORD) {
 }
 
 const mutex = new Mutex();
+/**
+ * Global is used here to maintain a cached connection across hot reloads
+ * in development. This prevents connections growing exponentially
+ * during API Route usage.
+ */
 // @ts-ignore
 let cached = global.mongoose;
 
 if (!cached) {
-  cached = { conn: null, promise: null, mongoServer: null };
+  cached = { conn: null, promise: null };
   // @ts-ignore
-  global.mongoose = cached;
+  global.mongoose = { conn: null, promise: null };
 }
+
+let seeded = false;
 
 const connect = async () => {
   const release = await mutex.acquire();
-
   try {
-    // Check if we're already connected or currently connecting to any kind of DB
     if (cached.conn) {
       return cached.conn;
     }
 
     if (!cached.promise) {
-      let connectionString;
-
-      if (process.env.USE_MOCK_DB) {
-        console.log("USING MOCK DB");
-        // If using a mock DB, initiate MongoMemoryServer
-        if (!cached.mongoServer) {
-          cached.mongoServer = await MongoMemoryServer.create();
-        }
-        connectionString = cached.mongoServer.getUri();
+      let connectionString = "";
+      if (MONGO_CONNECTION_STRING) {
+        connectionString = MONGO_CONNECTION_STRING;
       } else {
-        console.log("USING REAL DB");
-        // Else construct the real DB connection string
-        connectionString = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?authSource=admin`;
+        connectionString = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/${MONGO_DB}?authsource=admin`;
       }
 
-      cached.promise = mongoose.connect(connectionString).then((mongoose) => {
-        console.log(
-          `Connected to ${process.env.USE_MOCK_DB ? "Mock" : "Mongo"} DB`
-        );
-        return mongoose;
-      });
+      cached.promise = mongoose.connect(connectionString).then((res) => res);
+      console.log(connectionString);
+      console.trace("CONNECTED TO MONGO");
     }
 
     cached.conn = await cached.promise;
